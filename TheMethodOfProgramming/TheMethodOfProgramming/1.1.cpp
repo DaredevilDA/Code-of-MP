15af4b947bdd11022f15b2254f1c50882d0af867
DaredevilDA <wangxianyu04@gmail.com>
2016-04-25 19:28:55 +0800

    2016/4/25,commit for your birthday,happy birthday!

--- TheMethodOfProgramming/TheMethodOfProgramming/1.1.cpp
+++ TheMethodOfProgramming/TheMethodOfProgramming/1.1.cpp
@@ -2,35 +2,6 @@
 using namespace std;
 
 //字符串的旋转
-int _tmain(int argc, _TCHAR* argv[])
-{
-	return 0;
-}
-
-
-
-//蛮力移位--TS：O（mn） SS：O（1）
-void LeftShiftOne(char* s, int n)
-{
-	//保存第一个字符
-	char t = s[0];
-	for (int i = 0; i < n;i++)
-	{
-		s[i - 1] = s[i];
-	}
-	s[n - 1] = t;
-}
-
-void LeftRotateString(char* s, int n, int m)
-{
-	while (m--)
-	{
-		LeftShiftOne(s,n);
-	}
-}
-
-
-
 //三步反转
 void ReverseString(char* s, int from, int to)
 {
@@ -46,8 +17,47 @@ void ReverseString(char* s, int from, int to)
 void LeftRotateString(char* s, int n, int m)
 {
 	//若要左移动大于n位，那么于%n是等价的
-	m %= n;
-	ReverseString(s, 0, m - 1);
-	ReverseString(s, m, n - 1);
+	//m %= n;
 	ReverseString(s, 0, n - 1);
+	cout<<s<<endl;
+	ReverseString(s, n, m );
+	cout<<s<<endl;
+	ReverseString(s, 0, m );
+	cout<<s<<endl;
 }
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char test[10]="abcdef";
+	LeftRotateString(test,3,5);
+	cout<<test<<endl;
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+//蛮力移位--TS：O（mn） SS：O（1）
+//void LeftShiftOne(char* s, int n)
+//{
+//	//保存第一个字符
+//	char t = s[0];
+//	for (int i = 0; i < n;i++)
+//	{
+//		s[i - 1] = s[i];
+//	}
+//	s[n - 1] = t;
+//}
+//
+//void LeftRotateString(char* s, int n, int m)
+//{
+//	while (m--)
+//	{
+//		LeftShiftOne(s,n);
+//	}
+//}
+
--- TheMethodOfProgramming/TheMethodOfProgramming/1.2.cpp
+++ TheMethodOfProgramming/TheMethodOfProgramming/1.2.cpp
@@ -1,93 +1,93 @@
 #include "stdafx.h"
-using namespace std;
-
-//字符串的包含
-int _tmain(int argc, _TCHAR* argv[])
-{
-	return 0;
-}
-
-
-
-//蛮力轮询
-bool StringContain1(string &a, string &b)
-{
-	for (int i = 0; i < b.length(); ++i)
-	{
-		int j;
-		for (j = 0; (j < a.length()) && (a[j] != b[i]); ++j)
-			;
-		if (j >= a.length())
-		{
-			return false;
-		}
-	}
-	return true;
-}
-
-
-//排序后轮询
-bool StringContain2(string &a, string &b)
-{
-	sort(a.begin(),a.end());
-	sort(b.begin(),b.end());
-	for (int pa = 0, pb = 0; pb < b.length();)
-	{
-		while ((pa < a.length()) && (a[pa] < b[pb]))
-		{
-			++pa;
-		}
-		if ((pa<a.length()) || (a[pa]>b[pb]))
-		{
-			return false;
-		}
-		++pb;
-	}
-	return true;
-}
-
-
-//素数相乘
-bool StringContain2(string &a, string &b)
-{
-	const int p[26] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101};
-	int f = 1;
-	for (int i = 0; i < a.length(); ++i)
-	{
-		int x = p[a[i]-'A'];
-		if (f%x)//因为是检查b中的字符串是否都在a中，所以a中重复的字符找出一个代表即可
-		{
-			f *= x;
-		}
-	}
-	for (int i = 0; i < b.length(); ++i)
-	{
-		int x = p[b[i] - 'A'];
-		if (f%x)
-		{
-			return false;
-		}
-	}
-	return true;
-}
-
-
-
-//位运算法
-bool StringContain2(string &a, string &b)
-{
-	int hash = 0;
-	for (int i = 0; i < a.length(); ++i)
-	{
-		hash |= (1 << (a[i] - 'A'));
-	}
-	for (int i = 0; i < b.length(); ++i)
-	{
-		if ((hash&(1 << (b[i] - 'A'))) == 0)
-		{
-			return false;
-		}
-	}
-	return true;
-}
-
+//using namespace std;
+//
+////字符串的包含
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//	return 0;
+//}
+//
+//
+//
+////蛮力轮询
+//bool StringContain1(string &a, string &b)
+//{
+//	for (int i = 0; i < b.length(); ++i)
+//	{
+//		int j;
+//		for (j = 0; (j < a.length()) && (a[j] != b[i]); ++j)
+//			;
+//		if (j >= a.length())
+//		{
+//			return false;
+//		}
+//	}
+//	return true;
+//}
+//
+//
+////排序后轮询
+//bool StringContain2(string &a, string &b)
+//{
+//	sort(a.begin(),a.end());
+//	sort(b.begin(),b.end());
+//	for (int pa = 0, pb = 0; pb < b.length();)
+//	{
+//		while ((pa < a.length()) && (a[pa] < b[pb]))
+//		{
+//			++pa;
+//		}
+//		if ((pa<a.length()) || (a[pa]>b[pb]))
+//		{
+//			return false;
+//		}
+//		++pb;
+//	}
+//	return true;
+//}
+//
+//
+////素数相乘
+//bool StringContain2(string &a, string &b)
+//{
+//	const int p[26] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101};
+//	int f = 1;
+//	for (int i = 0; i < a.length(); ++i)
+//	{
+//		int x = p[a[i]-'A'];
+//		if (f%x)//因为是检查b中的字符串是否都在a中，所以a中重复的字符找出一个代表即可
+//		{
+//			f *= x;
+//		}
+//	}
+//	for (int i = 0; i < b.length(); ++i)
+//	{
+//		int x = p[b[i] - 'A'];
+//		if (f%x)
+//		{
+//			return false;
+//		}
+//	}
+//	return true;
+//}
+//
+//
+//
+////位运算法
+//bool StringContain2(string &a, string &b)
+//{
+//	int hash = 0;
+//	for (int i = 0; i < a.length(); ++i)
+//	{
+//		hash |= (1 << (a[i] - 'A'));
+//	}
+//	for (int i = 0; i < b.length(); ++i)
+//	{
+//		if ((hash&(1 << (b[i] - 'A'))) == 0)
+//		{
+//			return false;
+//		}
+//	}
+//	return true;
+//}
+//
